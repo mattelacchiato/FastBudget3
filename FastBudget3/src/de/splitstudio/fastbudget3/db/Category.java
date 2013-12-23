b4bd15b993638af27c89aaf5224e3d5eac2b0059
mattelacchiato <mb@blau.de>
2013-12-23 17:18:46 +0100

    generalization not needed here.

--- FastBudget3/src/de/splitstudio/fastbudget3/db/Category.java
+++ FastBudget3/src/de/splitstudio/fastbudget3/db/Category.java
@@ -66,7 +66,6 @@ public class Category implements Comparable<Category> {
 	}
 
 	public int calcBudget() {
-		//TODO (Dec 22, 2013): put this in DateUtils!
 		Calendar lastMonth = DateUtils.createFirstDayOfMonth();
 		lastMonth.add(Calendar.MILLISECOND, -1);
 		return calcGrossBudget() - summarizeExpenditures(null, lastMonth.getTime());
