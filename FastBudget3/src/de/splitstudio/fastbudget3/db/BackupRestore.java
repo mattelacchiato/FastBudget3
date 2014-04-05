ee44cc2d8392568e82fa1bd9943496d752563ec9
mattelacchiato <mb@blau.de>
2014-04-05 10:12:08 +0200

    cleanup

--- FastBudget3/src/de/splitstudio/fastbudget3/db/BackupRestore.java
+++ FastBudget3/src/de/splitstudio/fastbudget3/db/BackupRestore.java
@@ -18,25 +18,41 @@ public class BackupRestore {
 	private static final String BACKUP_MIME_TYPE = "application/octet-stream";
 
 	public static void backup(final Context context) {
+		File dest = getFile(context, ".backup");
+		if (dest == null) {
+			return;
+		}
+
+		backup(context, dest);
+		sendFile(context, dest);
+	}
+
+	private static void sendFile(final Context context, final File dest) {
+		DialogHelper.createQuestion(context, R.string.success, R.string.warning_backup_created, R.string.cancel,
+			R.string.send_file, new Runnable() {
+				@Override
+				public void run() {
+					String chooserTitle = context.getString(R.string.send_file);
+					Intent intent = new Intent(ACTION_SEND);
+					intent.setType(BACKUP_MIME_TYPE);
+					intent.putExtra(EXTRA_STREAM, Uri.fromFile(dest));
+					context.startActivity(createChooser(intent, chooserTitle));
+				}
+			}, dest.getAbsolutePath());
+	}
+
+	private static void backup(final Context context, final File dest) {
+		Database.getInstance(context).ext().backupSync(dest.getAbsolutePath());
+	}
+
+	private static File getFile(Context context, String suffix) {
 		File externalFilesDir = context.getExternalFilesDir(null);
 		if (externalFilesDir == null) {
 			DialogHelper.createAlert(context, R.string.warning, R.string.warning_no_external_storage, R.string.ok);
-		} else {
-			String filename = context.getString(R.string.app_name) + ".backup";
-			final File dest = new File(externalFilesDir, filename);
-			Database.getInstance(context).ext().backupSync(dest.getAbsolutePath());
-			DialogHelper.createQuestion(context, R.string.success, R.string.warning_backup_created, R.string.cancel,
-				R.string.send_file, new Runnable() {
-					@Override
-					public void run() {
-						String chooserTitle = context.getString(R.string.send_file);
-						Intent intent = new Intent(ACTION_SEND);
-						intent.setType(BACKUP_MIME_TYPE);
-						intent.putExtra(EXTRA_STREAM, Uri.fromFile(dest));
-						context.startActivity(createChooser(intent, chooserTitle));
-					}
-				}, dest.getAbsolutePath());
+			return null;
 		}
+		String filename = context.getString(R.string.app_name) + suffix;
+		return new File(externalFilesDir, filename);
 	}
 
 }
